1396238417929bc4c4b714b07803b94043387931
ahmedisa106 <ahmedisa106@gmail.com>
2024-03-16 01:47:00 +0200

    fix order observer

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -162,6 +162,7 @@ class OrderObserver
 
             Notification::send($order->customer, new NewOrder($customer_notification));
 
+            @dd($order->customer->notifiable->token);
             Fcm::sendToTokens(
                 tokens: [$order->customer->notifiable->token],
                 title: $order->provider?->name ?? env('APP_NAME'),
