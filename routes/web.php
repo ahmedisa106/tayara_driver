5728a91c523c105f4dcc53025bba4ca4cd0ce179
ahmedisa106 <ahmedisa106@gmail.com>
2024-03-16 01:57:14 +0200

    fix order observer

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\Fcm\Fcm;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('fcm', function ($id) {
 
+    $res =  Fcm::sendToTokens(
+        ["fmmUO22JRqGjY8_cucvZj2:APA91bF_CeCBRlMJwSMmioklCjZTKRvhUWpkwekPzg7-nixjnFng7u2W3akdgJYe0kZIomnaAZqpHZbbdh97IdXzxGkn9jU0d4jatvjN8wqgqo0_ivF7L8YMw32V27G2aVEymCSP_Y_H"],
+        env('APP_NAME'),
+        "test message body"
+    );
+    dd($res);
+});
