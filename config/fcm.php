1fa3afa7eda74bdd3f881a82d902f29d407003f1
ahmedisa106 <ahmedisa106@gmail.com>
2024-03-16 02:03:03 +0200

    fix order observer

---
+++ config/fcm.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "server_key" => "AAAAeQbU_bU:APA91bFLhTaCD4eXmYDIlUYeOCrofavg-bkAsOpUe07YkAZ5SzLuVjvA_MNV5xJpdevgvPTAliTwl8QSH5CHFQWnlfZft9ODm8Gtpd2ob-rzOVpm0O0d1i-d7C-kb-2Dk72L16yj5etP"
+];
