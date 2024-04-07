f543e9b2673671c38c644bdb193fd461af505ba0
ahmedisa106 <ahmedisa106@gmail.com>
2024-04-07 19:12:08 +0200

    enhancment

--- app/Http/Controllers/Api/Shifts/ShiftController.php
+++ app/Http/Controllers/Api/Shifts/ShiftController.php
@@ -48,7 +48,8 @@ class ShiftController extends Controller
             $q->where('status', OrderStatus::Complete)
                 ->withCount('orders')
                 ->withSum('orders', 'driver_ratio');
-        })->where('driver_id', auth()->id());
+        })
+            ->where('driver_id', auth()->user()->id());
 
         dd($shift, 'sad');
         return $this->final_response(data: new ShiftResource($shift));
